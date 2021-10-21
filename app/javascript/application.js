c6bb2d49c662d9a6a438e49dba220e5fe05db2aa
Pawel Suchta <pawel.suchta@polcode.net>
2021-10-21 21:46:02 +0200

    Initialize Google Maps, load the maps script

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+window.initMap = function (...args) {
+  const event = document.createEvent("Events")
+  event.initEvent("google-maps-callback", true, true)
+  event.args = args
+  window.dispatchEvent(event)
+}
