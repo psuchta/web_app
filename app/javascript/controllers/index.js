6a2a6d8fb8f60f7cab3d307990fcae012005684d
Pawel Suchta <pawel.suchta@polcode.net>
2021-11-01 22:27:56 +0100

    Add address stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,5 +3,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import AddressesController from "./addresses_controller"
+application.register("addresses", AddressesController)
