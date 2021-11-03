3d7bff7a28a35b356fbf4cfe3e6912ee0359bf87
Pawel Suchta <pawel.suchta@polcode.net>
2021-11-03 23:21:53 +0100

    Create address new page

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,5 +1,23 @@
 class AddressesController < ApplicationController
-
   def index
+  end
+
+  def new
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+    if @address.save!
+      render :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :latitude, :longtitude, :description, :address)
   end
 end
