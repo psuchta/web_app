a843f08d30d691ddc076257dd8ca4c77257682a0
Pawel Suchta <pawel.suchta@polcode.net>
2021-11-21 20:44:47 +0100

    Adjust stimulus addresses for edit page

--- app/javascript/controllers/addresses_controller.js
+++ app/javascript/controllers/addresses_controller.js
@@ -10,24 +10,46 @@ export default class extends Controller {
   }
 
   initMap() {
+    const init_latitude = this.data.get('latitude')
+    const init_longtitude = this.data.get('longtitude')
+
     if (this._map === undefined) {
-      this._map = new google.maps.Map(this.mapTarget, {
-        center: new google.maps.LatLng(
-          39.5,
-          -98.35
-        ),
-        zoom: 4,
-      });
+      if (init_latitude && init_longtitude){
+        const myLatLong= new google.maps.LatLng(init_latitude,init_longtitude);
+
+        this._map = new google.maps.Map(this.mapTarget, {
+          center: myLatLong,
+          zoom: 14,
+        });
+
+        this.marker = new google.maps.Marker({
+          map: this._map,
+          anchorPoint: new google.maps.Point(52, 19)
+        });
+
+        // Set visible Marker
+        this.marker.setPosition(myLatLong);
+        this.marker.setVisible(true);
+      }
+      else {
+        this._map = new google.maps.Map(this.mapTarget, {
+          center: new google.maps.LatLng(
+            52.5,
+            19.35
+          ),
+          zoom: 6,
+        });
+
+        this.marker = new google.maps.Marker({
+          map: this._map,
+          anchorPoint: new google.maps.Point(52, 19)
+        });
+      }
       this.autocomplete = new google.maps.places.Autocomplete(this.fieldTarget);
       this.autocomplete.bindTo('bounds', this._map);
       this.autocomplete.setFields(['address_component', 'geometry', 'icon', 'name']);
       this.autocomplete.addListener('place_changed', this.placeChanged.bind(this));
     }
-    this.marker = new google.maps.Marker({
-      map: this._map,
-      anchorPoint: new google.maps.Point(0, -29)
-    });
-
     return this._map;
   }
 
