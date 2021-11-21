abb4b9025a569d59865159113164d3beaeb89d9b
Pawel Suchta <pawel.suchta@polcode.net>
2021-11-21 20:45:56 +0100

    Add edit and index page for addresses

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,9 +1,15 @@
 class AddressesController < ApplicationController
+  before_action :find_address, only: [:edit, :update]
+
   def index
+    @addresses = Address.all
   end
 
   def new
     @address = Address.new
+  end
+
+  def edit
   end
 
   def create
@@ -15,7 +21,16 @@ class AddressesController < ApplicationController
     end
   end
 
+  def update
+    @address.update!(address_params)
+    redirect_to addresses_path
+  end
+
   private
+
+  def find_address
+    @address = Address.find(params[:id])
+  end
 
   def address_params
     params.require(:address).permit(:name, :latitude, :longtitude, :description, :address)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
-  resources :addresses, only: [:index, :new, :create]
+  resources :addresses, only: [:index, :new, :edit, :update, :create]
 end
