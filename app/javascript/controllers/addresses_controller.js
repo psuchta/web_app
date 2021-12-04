d780c1352939ec8b51e1b953f24f649114113fc4
Pawel Suchta <pawel.suchta@polcode.net>
2021-12-04 19:26:20 +0100

    Fix eslint issues for es12

--- app/javascript/controllers/addresses_controller.js
+++ app/javascript/controllers/addresses_controller.js
@@ -1,89 +1,93 @@
-import { Controller } from "@hotwired/stimulus"
-import { MarkerClusterer } from "@googlemaps/markerclusterer";
-
+import { Controller } from '@hotwired/stimulus';
+import { MarkerClusterer } from '@googlemaps/markerclusterer';
 
 export default class extends Controller {
-  static targets = ['map', 'field', 'latitude', 'longtitude']
+  static targets = ['map', 'field', 'latitude', 'longtitude'];
+
   static values = {
-      addresses: Array
-    }
+    addresses: Array,
+  };
 
   connect() {
-    if (typeof google != "undefined") {
+    if (typeof google !== 'undefined') {
       this.initMap();
     }
   }
 
   showAllAddresses() {
-    if(this._map !== undefined) return this._map
+    const { google } = window;
 
-   this.addressesValue = this.addressesValue.map((position)=> {
-    position.lat = parseFloat(position.lat);
-    position.lng = parseFloat(position.lng);
-    return position;
-   })
+    if (this.map !== undefined) return this.map;
 
-   const locations = this.addressesValue.filter(({lat, lng}) => {
-    return lat || lng;
-   });
+    this.addressesValue = this.addressesValue.map((position) => {
+      const newPosition = {};
+      newPosition.lat = parseFloat(position.lat);
+      newPosition.lng = parseFloat(position.lng);
+      return newPosition;
+    });
 
-    this._map = new google.maps.Map(this.mapTarget, {
+    const locations = this.addressesValue.filter(({ lat, lng }) => lat || lng);
+
+    this.map = new google.maps.Map(this.mapTarget, {
       center: new google.maps.LatLng(
         52.5,
-        19.35
+        19.35,
       ),
       zoom: 6,
     });
-  
+
     const infoWindow = new google.maps.InfoWindow();
 
-    const markers = locations.map(({address, description, ...position}, i) => {
+    const markers = locations.map(({ address, description, ...position }) => {
       const marker = new google.maps.Marker({
-        position
+        position,
       });
       const contentString = this.addressInfoWindow(address, description);
 
-      marker.addListener("click", () => {
+      marker.addListener('click', () => {
         infoWindow.setContent(contentString);
-        infoWindow.open(this._map, marker);
+        infoWindow.open(this.map, marker);
       });
       return marker;
     });
 
     // Add a marker clusterer to manage the markers.
-    new MarkerClusterer({ markers: markers, map: this._map });
+    /* eslint-disable no-new */
+    new MarkerClusterer({ markers, map: this.map });
+    return this.map;
   }
 
   initMap() {
-    const init_latitude = this.data.get('latitude')
-    const init_longtitude = this.data.get('longtitude')
+    const { google } = window;
+    const initLatitude = this.data.get('latitude');
+    const initLongtitude = this.data.get('longtitude');
 
-    if(this._map !== undefined) return this._map
+    if (this.map !== undefined) return this.map;
 
-    if (init_latitude && init_longtitude){
-      this.init_for_edit(init_latitude, init_longtitude);
-    }
-    else {
+    if (initLatitude && initLongtitude) {
+      this.init_for_edit(initLatitude, initLongtitude);
+    } else {
       this.init_blank_map();
     }
     this.autocomplete = new google.maps.places.Autocomplete(this.fieldTarget);
-    this.autocomplete.bindTo('bounds', this._map);
+    this.autocomplete.bindTo('bounds', this.map);
     this.autocomplete.setFields(['address_component', 'geometry', 'icon', 'name']);
     this.autocomplete.addListener('place_changed', this.placeChanged.bind(this));
-    return this._map;
+    return this.map;
   }
 
   init_for_edit(latitude, longtitude) {
-    const myLatLong= new google.maps.LatLng(latitude,longtitude);
+    const { google } = window;
+    const myLatLong = new google.maps.LatLng(latitude, longtitude);
 
-    this._map = new google.maps.Map(this.mapTarget, {
+    this.map = new google.maps.Map(this.mapTarget, {
       center: myLatLong,
       zoom: 14,
     });
 
     this.marker = new google.maps.Marker({
-      map: this._map,
-      anchorPoint: new google.maps.Point(52, 19)
+      map: this.map,
+      anchorPoint: new google.maps.Point(52, 19),
     });
 
     // Set visible Marker
@@ -92,33 +96,34 @@ export default class extends Controller {
   }
 
   init_blank_map() {
-    this._map = new google.maps.Map(this.mapTarget, {
+    const { google } = window;
+    this.map = new google.maps.Map(this.mapTarget, {
       center: new google.maps.LatLng(
         52.5,
-        19.35
+        19.35,
       ),
       zoom: 6,
     });
 
     this.marker = new google.maps.Marker({
-      map: this._map,
-      anchorPoint: new google.maps.Point(52, 19)
+      map: this.map,
+      anchorPoint: new google.maps.Point(52, 19),
     });
   }
 
   placeChanged() {
-    let place = this.autocomplete.getPlace();
+    const place = this.autocomplete.getPlace();
 
     if (!place.geometry) {
       window.alert(`No details available for: ${place.name}`);
       return;
     }
 
-    if(place.geometry.viewport){
-      this._map.fitBounds(place.geometry.viewport);
+    if (place.geometry.viewport) {
+      this.map.fitBounds(place.geometry.viewport);
     } else {
-      this._map.setCenter(place.geometry.location);
-      this._map.setZoom(17);
+      this.map.setCenter(place.geometry.location);
+      this.map.setZoom(17);
     }
 
     this.marker.setPosition(place.geometry.location);
@@ -129,18 +134,18 @@ export default class extends Controller {
   }
 
   keydown(event) {
-    if (event.key == 'Enter') {
+    if (event.key === 'Enter') {
       event.preventDefault();
     }
   }
 
   addressInfoWindow(address, description) {
-    const descriptionHtml  = description ? "<p> <b>Description: </b>" + description + "</p>" : '';
-    return '<div id="content">' +
-           '<div id="bodyContent">' +
-           "<p> <b>Address: </b>" + address + "</p>"+
-           descriptionHtml +
-           "</div>" +
-           "</div>";
+    const descriptionHtml = description ? `<p> <b>Description: </b>${description}</p>` : '';
+    return `${'<div id="content">'
+           + '<div id="bodyContent">'
+           + '<p> <b>Address: </b>'}${address}</p>${
+      descriptionHtml
+    }</div>`
+           + '</div>';
   }
 }
